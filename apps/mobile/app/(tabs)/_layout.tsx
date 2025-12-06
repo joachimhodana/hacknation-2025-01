51cfa88b63ec727a5da37883a848d22f6795cf4c
kluczi <bartekcsgo04@gmail.com>
2025-12-06 15:26:14 +0100

    add login and start view

--- apps/mobile/app/(tabs)/_layout.tsx
+++ apps/mobile/app/(tabs)/_layout.tsx
@@ -30,6 +30,20 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="login"
+        options={{
+          title: 'Login',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="lock.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="start"
+        options={{
+          title: 'Start',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="lock.fill" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
