53e317dccfb746c0652874f049de8d4675f7a8ca
style77 <stylek777@gmail.com>
2025-12-06 11:58:14 +0100

    init better auth base with prisma

--- apps/core/src/index.ts
+++ apps/core/src/index.ts
@@ -1,7 +1,4 @@
 import { Elysia } from "elysia";
+import { auth } from "./auth";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
-
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+const app = new Elysia().mount(auth.handler)
