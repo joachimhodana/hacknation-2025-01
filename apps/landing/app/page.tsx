c1c7a3f7d31a4b939f6d2e97dd4c07a6f5ec0a64
th11n <thinex@wp.pl>
2025-12-06 22:39:25 +0100

    chore: landing page structure

--- apps/landing/app/page.tsx
+++ apps/landing/app/page.tsx
@@ -1,11 +1,23 @@
+'use client'
+
+import DownloadCta from "@/components/cta";
+import FAQ from "@/components/faq";
+import FeaturesSection from "@/components/features";
+import Footer from "@/components/footer";
 import Hero from "@/components/hero";
+import HowItWorks from "@/components/how-it-works";
 import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center w-full">
+    <div className="min-h-screen w-full font-sans">
       <Navbar />
       <Hero />
+      <FeaturesSection />
+      <HowItWorks />
+      <FAQ />
+      <DownloadCta />
+      <Footer />
     </div>
   );
 }
